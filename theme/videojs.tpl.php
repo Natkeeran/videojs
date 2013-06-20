a0f97b72963840032f6b5ef24a3a2664d17af564
Jorrit Schippers <jorrit@161217.no-reply.drupal.org>
2013-06-20 22:42:44 +0200

    Issue #2007342 by Jorrit: Fixed JSON parse error

--- theme/videojs.tpl.php
+++ theme/videojs.tpl.php
@@ -13,7 +13,10 @@ if (!empty($poster)) {
 }
 
 if (!empty($items)): ?>
-<video id="<?php print $player_id; ?>-video" data-setup="{'width': <?php echo $width ?>, 'height': <?php echo $height ?>}" class="video-js vjs-default-skin" width="<?php print($width) ?>" height="<?php print($height) ?>" controls="controls" preload="auto"<?php echo $attrs; ?>>
+<video id="<?php print $player_id; ?>-video" class="video-js vjs-default-skin" width="<?php print($width) ?>" height="<?php print($height) ?>" controls="controls" preload="auto"<?php echo $attrs; ?> data-setup='{
+  "width": <?php echo $width ?>,
+  "height": <?php echo $height ?>
+}'>
 <?php foreach ($items as $item): ?>
   <source src="<?php print check_plain(file_create_url($item['uri'])) ?>" type="<?php print check_plain($item['videotype']) ?>" />
 <?php endforeach; ?>
